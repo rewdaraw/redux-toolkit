ed0931df0bb12cad15152994b1190e140052fee9
RT <rewdaraw@gmail.com>
2022-01-29 03:09:32 +0900

    wip

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchAsyncLogin,
+  fetchAsyncRegister,
+  fetchAsyncProfile,
+  editUsername,
+  editPassword,
+  toggleMode,
+  selectAuthen,
+  selectIsLoginView,
+  selectProfile,
+} from "./loginSlice";
+import styles from "./Login.module.css";
 
 const Login = () => {
-  return <div>Hello, Login</div>;
+  const a = "gsgs";
+  const b = "gdg";
+  const btnDisabler = a === "" || b === "";
+  console.log({ btnDisabler });
+  return (
+    <div>
+      ここから先は暇な時に自分でログイン有りのTODOアプリを作ってみる方が勉強になるので一旦中止
+    </div>
+  );
 };
 
 export default Login;
